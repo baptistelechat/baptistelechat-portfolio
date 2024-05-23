c0fc294713a919da7ee2bbd6a84384d3b79d5a64
Baptiste LECHAT <baptistelechat@outlook.fr>
2024-05-24 00:56:39 +0200

    🚧♻️ (Grid) - Start to recreate bento grid structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,98 @@
-import AvatarTile from "@/components/AvatarTile";
-import HobbyTile from "@/components/HobbyTile/HobbyTile";
-import LogoTile from "@/components/LogoTile";
-import ProjectTile from "@/components/ProjectTile";
+import { BentoCard, BentoGrid } from "@/components/magicui/bento-grid";
+import {
+  BellIcon,
+  CalendarIcon,
+  FileTextIcon,
+  GlobeIcon,
+  TextCursorInput,
+} from "lucide-react";
+import Image from "next/image";
 
-export default function Home() {
+const features = [
+  {
+    Icon: FileTextIcon,
+    name: "Save your files",
+    description: "We automatically save your files as you type.",
+    href: "/",
+    cta: "Learn more",
+    background: (
+      <Image
+        src=""
+        className="absolute -right-20 -top-20 opacity-60"
+        alt="profile card"
+      />
+    ),
+    className: "lg:row-start-1 lg:row-end-4 lg:col-start-2 lg:col-end-3",
+  },
+  {
+    Icon: TextCursorInput,
+    name: "Full text search",
+    description: "Search through all your files in one place.",
+    href: "/",
+    cta: "Learn more",
+    background: (
+      <Image
+        src=""
+        className="absolute -right-20 -top-20 opacity-60"
+        alt="profile card"
+      />
+    ),
+    className: "lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3",
+  },
+  {
+    Icon: GlobeIcon,
+    name: "Multilingual",
+    description: "Supports 100+ languages and counting.",
+    href: "/",
+    cta: "Learn more",
+    background: (
+      <Image
+        src=""
+        className="absolute -right-20 -top-20 opacity-60"
+        alt="profile card"
+      />
+    ),
+    className: "lg:col-start-1 lg:col-end-2 lg:row-start-3 lg:row-end-4",
+  },
+  {
+    Icon: CalendarIcon,
+    name: "Calendar",
+    description: "Use the calendar to filter your files by date.",
+    href: "/",
+    cta: "Learn more",
+    background: (
+      <Image
+        src=""
+        className="absolute -right-20 -top-20 opacity-60"
+        alt="profile card"
+      />
+    ),
+    className: "lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:row-end-2",
+  },
+  {
+    Icon: BellIcon,
+    name: "Notifications",
+    description:
+      "Get notified when someone shares a file or mentions you in a comment.",
+    href: "/",
+    cta: "Learn more",
+    background: (
+      <Image
+        src=""
+        className="absolute -right-20 -top-20 opacity-60"
+        alt="profile card"
+      />
+    ),
+    className: "lg:col-start-3 lg:col-end-3 lg:row-start-2 lg:row-end-4",
+  },
+];
+
+export default async function BentoDemo() {
   return (
-    <main className="grid grid-cols-4 grid-rows-6 gap-6 p-6">
-      {/* <ModeToggle /> */}
-      <div className="col-span-2 row-span-2 ">
-        <AvatarTile
-          fullName="Baptiste LECHAT"
-          description="Dessinateur / Développeur / Responsable Réseau sociaux"
-          avatar="avatar"
-        />
-      </div>
-      <div className="col-start-3 row-start-1 ">
-        <LogoTile
-          title="React"
-          description="Javascript Framework"
-          logo="react"
-        />
-      </div>
-      <div className="col-start-4 row-start-1 ">
-        <LogoTile title="NextJS" description="React Framework" logo="next" />
-      </div>
-      <div className="col-start-3 row-start-2 ">
-        <LogoTile
-          title="TypeScript"
-          description="Programming language"
-          logo="typescript"
-        />
-      </div>
-      <div className="col-start-4 row-start-2 ">
-        <LogoTile
-          title="NodeJS"
-          description="JavaScript runtime"
-          logo="nodejs"
-        />
-      </div>
-      <div className="col-span-2 row-start-3 ">
-        <ProjectTile
-          title="Sismo"
-          description="Un outil pour obtenir les zones de neige, de vent et de sismicité en France (incluant DROM-COM)."
-          pattern="pattern-topography-indigo-800/50"
-          logo="sismo"
-          stack1={["react", "javascript", "material_ui", "nodejs"]}
-          stack2={["firebase", "vercel"]}
-        />
-      </div>
-      <div className="col-span-2 col-start-3 row-start-3 ">
-        <ProjectTile
-          title="SpeedTest Tracker"
-          description="Une application web pour surveiller les données des tests de vitesse internet sur un réseau local."
-          pattern="pattern-circuit-board-slate-700/50"
-          emoji="🚀"
-          stack1={["react_ts", "typescript", "shadcn_ui_", "nodejs"]}
-          stack2={["tailwindcss", "docker"]}
-        />
-      </div>
-      <div className="col-span-2 row-start-4 ">
-        <ProjectTile
-          title="Swish"
-          description="Un système d'automatisation de publication des résultats du basket français."
-          pattern="pattern-tic-tac-toe-orange-800/50"
-          emoji="🏀"
-          stack1={["react_ts", "typescript", "shadcn_ui_", "nodejs"]}
-          stack2={["tailwindcss", "notion_"]}
-        />
-      </div>
-      <div className="col-span-2 col-start-3 row-start-4 ">
-        <ProjectTile
-          title="Social template (123 Structure)"
-          description="Une application web pour générer des visuels pour le compte LinkedIn de la société : 123 Structure."
-          pattern="pattern-wiggle-yellow-800/50"
-          logo="123str"
-          stack1={["react", "javascript", "material_ui", "vercel"]}
-        />
-      </div>
-      <div className="row-start-5 ">
-        <HobbyTile index={1} title="Développement" />
-      </div>
-      <div className="row-start-5 ">
-        <HobbyTile index={2} title="Sports" />
-      </div>
-      <div className="row-start-5 ">
-        <HobbyTile index={3} title="Jeux vidéo" />
-      </div>
-      <div className="row-start-5 ">
-        <HobbyTile index={4} title="Sports mécaniques" />
-      </div>
-      <div className="col-span-4 row-start-6 rounded-3xl bg-sky-900" />
-    </main>
+    <BentoGrid className="lg:grid-rows-3">
+      {features.map((feature) => (
+        <BentoCard key={feature.name} {...feature} />
+      ))}
+    </BentoGrid>
   );
 }

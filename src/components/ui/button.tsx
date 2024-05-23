2d421f246d72b44e9c4380135b98ca06d5aa1b7c
Baptiste LECHAT <baptistelechat@outlook.fr>
2024-05-24 00:56:39 +0200

    🚧♻️ (Grid) - Start to recreate bento grid structure

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,9 @@
+"use client";
+import { cn } from "@/lib/utils";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
+import { motion } from "framer-motion";
 import * as React from "react";
-
-import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium shadow-sm shadow-sky-400/50 ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -53,4 +54,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 );
 Button.displayName = "Button";
 
-export { Button, buttonVariants };
+const MotionButton = motion(Button);
+
+export { Button, MotionButton, buttonVariants };

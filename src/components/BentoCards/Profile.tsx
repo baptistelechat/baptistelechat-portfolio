9e94e65a68a5a05d85c052de6a1f76c353b607c6
Baptiste LECHAT <baptistelechat@outlook.fr>
2024-06-04 23:47:06 +0200

    📱 (Responsive) - Work on responsive design

--- src/components/BentoCards/Profile.tsx
+++ src/components/BentoCards/Profile.tsx
@@ -32,7 +32,8 @@ const Profile = (): BentoCardProps => {
         />
       </>
     ),
-    className: "lg:row-start-1 lg:row-end-2 lg:col-start-1 lg:col-end-3",
+    className:
+      "row-start-1 row-end-2 col-start-1 col-end-2 sm:row-start-1 sm:row-end-2 sm:col-start-1 sm:col-end-2 lg:row-start-1 lg:row-end-2 lg:col-start-1 lg:col-end-3",
   };
 };
 
--- src/components/SocialDock.tsx
+++ src/components/SocialDock.tsx
@@ -34,7 +34,7 @@ const SocialDock = () => {
   return (
     <div
       className={cn(
-        "absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100 justify-end"
+        "absolute bottom-0 flex w-full translate-y-0 lg:translate-y-10 transform-gpu flex-row items-center p-4 opacity-100 lg:opacity-0 transition-all duration-300 lg:group-hover:translate-y-0 lg:group-hover:opacity-100 justify-end"
       )}
     >
       <Dock>
